6292574d479c092059fedb2fb43724c56ac7447b
Stephanie Spielman <stephanie.spielman@gmail.com>
2015-08-22 18:05:39 -0500

    script for computing dnds from alignment frequencies

--- scripts/predict_dnds_from_alignments.py
+++ scripts/predict_dnds_from_alignments.py
@@ -1,35 +1,78 @@
 # SJS
-# Here, we predict site-specific dN/dS values from the frequencies in the alignment columns (using true kappa)
+# Here, we predict site-specific dN/dS values from the frequencies in the alignment columns.
+# We need to use the mutation rates as inferred by HyPhy for this, as well.
 
-import os
-import sys
-sys.path.append("/Users/sjspielman/Research/sitewise_dnds_mutsel_exp/scripts/simulation")
-from dnds_mutsel_functions import *
-import numpy as np
+from compute_dnds_from_mutsel import *
+from pyvolve import Genetics
 from Bio import AlignIO
-import multiprocessing
-
-# GLOBALS, HUZZAH
-numproc = 3 # processors
-resdir = "/Users/sjspielman/Research/mutsel_bench/results/"
-seqdir = "/Users/sjspielman/Research/mutsel_bench/data/"
-
-mu = 1.
-kappa = 4.0
-mu_dict = {'AT': mu, 'TA':mu, 'CG': mu, 'GC':mu, 'AC': mu, 'CA':mu, 'GT':mu, 'TG':mu, 'AG': kappa*mu, 'GA':kappa*mu, 'CT':kappa*mu, 'TC':kappa*mu}
-codons=["AAA", "AAC", "AAG", "AAT", "ACA", "ACC", "ACG", "ACT", "AGA", "AGC", "AGG", "AGT", "ATA", "ATC", "ATG", "ATT", "CAA", "CAC", "CAG", "CAT", "CCA", "CCC", "CCG", "CCT", "CGA", "CGC", "CGG", "CGT", "CTA", "CTC", "CTG", "CTT", "GAA", "GAC", "GAG", "GAT", "GCA", "GCC", "GCG", "GCT", "GGA", "GGC", "GGG", "GGT", "GTA", "GTC", "GTG", "GTT", "TAC", "TAT", "TCA", "TCC", "TCG", "TCT", "TGC", "TGG", "TGT", "TTA", "TTC", "TTG", "TTT"]
-amino_acids  = ["A", "C", "D", "E", "F", "G", "H", "I", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "V", "W", "Y"]
-codons=["AAA", "AAC", "AAG", "AAT", "ACA", "ACC", "ACG", "ACT", "AGA", "AGC", "AGG", "AGT", "ATA", "ATC", "ATG", "ATT", "CAA", "CAC", "CAG", "CAT", "CCA", "CCC", "CCG", "CCT", "CGA", "CGC", "CGG", "CGT", "CTA", "CTC", "CTG", "CTT", "GAA", "GAC", "GAG", "GAT", "GCA", "GCC", "GCG", "GCT", "GGA", "GGC", "GGG", "GGT", "GTA", "GTC", "GTG", "GTT", "TAC", "TAT", "TCA", "TCC", "TCG", "TCT", "TGC", "TGG", "TGT", "TTA", "TTC", "TTG", "TTT"]
-codon_dict = {"AAA":"K", "AAC":"N", "AAG":"K", "AAT":"N", "ACA":"T", "ACC":"T", "ACG":"T", "ACT":"T", "AGA":"R", "AGC":"S", "AGG":"R", "AGT":"S", "ATA":"I", "ATC":"I", "ATG":"M", "ATT":"I", "CAA":"Q", "CAC":"H", "CAG":"Q", "CAT":"H", "CCA":"P", "CCC":"P", "CCG":"P", "CCT":"P", "CGA":"R", "CGC":"R", "CGG":"R", "CGT":"R", "CTA":"L", "CTC":"L", "CTG":"L", "CTT":"L", "GAA":"E", "GAC":"D", "GAG":"E", "GAT":"D", "GCA":"A", "GCC":"A", "GCG":"A", "GCT":"A", "GGA":"G", "GGC":"G", "GGG":"G", "GGT":"G", "GTA":"V", "GTC":"V", "GTG":"V", "GTT":"V", "TAC":"Y", "TAT":"Y", "TCA":"S", "TCC":"S", "TCG":"S", "TCT":"S", "TGC":"C", "TGG":"W", "TGT":"C", "TTA":"L", "TTC":"F", "TTG":"L", "TTT":"F"}
-genetic_code = [["GCA", "GCC", "GCG", "GCT"], ["TGC","TGT"], ["GAC", "GAT"], ["GAA", "GAG"], ["TTC", "TTT"], ["GGA", "GGC", "GGG", "GGT"], ["CAC", "CAT"], ["ATA", "ATC", "ATT"], ["AAA", "AAG"], ["CTA", "CTC", "CTG", "CTT", "TTA", "TTG"], ["ATG"], ["AAC", "AAT"], ["CCA", "CCC", "CCG", "CCT"], ["CAA", "CAG"], ["AGA", "AGG", "CGA", "CGC", "CGG", "CGT"] , ["AGC", "AGT", "TCA", "TCC", "TCG", "TCT"], ["ACA", "ACC", "ACG", "ACT"], ["GTA", "GTC", "GTG", "GTT"], ["TGG"], ["TAC", "TAT"]]
+import numpy as np
+import os
+ZERO=1e-8
+g = Genetics()
 
 
+def build_mu_dict(pi, kappa):
+    ''' Construct mutation rates dictionary.'''
+    a = pi[0]
+    c = pi[1]
+    g = pi[2]
+    t = pi[3]
+    mu = {'AG':kappa*g, 'TC':kappa*c, 'GA':kappa*a, 'CT':kappa*t, 'AC':c, 'TG':g, 'CA':a, 'GT':t, 'AT':t, 'TA':a, 'GC':c, 'CG':g}  
+    return mu
 
-def compute_dnds(d):
+
+def extract_mutation_rates(hyphile): # hehe argument!
+
+    with open(hyphile, "r") as f:
+        hyout_string = f.read()
+        
+    # Nucleotide frequencies #
+    nuc_freqs = np.zeros(4)
+    find_aux_1 = re.search("aux_1=(0\.\d+)", hyout_string)
+    aux_1 = float( find_aux_1.group(1) )
+    find_aux_2 = re.search("aux_2=(0\.\d+)", hyout_string)
+    aux_2 = float( find_aux_2.group(1) )
+    find_aux_3 = re.search("aux_3=(0\.\d+)", hyout_string)
+    aux_3 = float( find_aux_3.group(1) )
+    nuc_freqs[0] = aux_1
+    nuc_freqs[1] = (1. - aux_1) * aux_2
+    nuc_freqs[2] = (1. - aux_1) * (1. - aux_2) * aux_3
+    nuc_freqs[3] = (1. - aux_1) * (1. - aux_2) * (1. - aux_3)
+    assert( abs(1. - np.sum(nuc_freqs)) < ZERO ), "Nucleotide frequencies do not sum to 1!"
+    nuc_freqs_ordered = nuc_freqs[[3,1,0,2]]
+
+    # Kappa #
+    find_kappa = re.search("k=(\d+\.\d+)", hyout_string)
+    kappa = find_kappa.group(1)
+    
+    # Mutation dictionary #
+    return build_mu_dict(nuc_freqs, float(kappa) )
+
+
+
+def aa_to_codon_freqs(aa_freqs):
+    
+    codon_freqs = np.zeros(61)
+    aa_dict = dict(zip(g.amino_acids, aa_freqs))
+    for aa in aa_dict:
+        syn_codons = g.genetic_code[ g.amino_acids.index(aa) ]
+        cf = aa_dict[aa] / float(len(syn_codons))
+        for syn in syn_codons:
+            codon_freqs[ g.codons.index(syn) ] = cf
+    
+    assert(1. - np.sum(codon_freqs) <= ZERO), "bad codon freqs from aa freqs"
+    return codon_freqs
+
+
+
+
+
+def compute_dnds(d, mu_dict):
+    c = dNdS_from_MutSel(d, mu_dict)
     dnds = None
     # Assertion error will be raised in dnds calculation if no evolution has occurred. These are uninformative sites.
     try:
-        dnds = derive_dnds( d, mu_dict )
+        dnds = c.compute_dnds()
         if np.isnan(dnds) or np.isinf(dnds):
             dnds = "NA"
     except AssertionError:
@@ -39,94 +82,82 @@ def compute_dnds(d):
     return dnds
 
 
-def multi_worker(name):
-    '''
-        Wrapper function for computing dnds in multiprocessing framework.
-    '''
-    alnfile = seqdir + name + ".phy"
-    outfile = resdir + name + "_predicted_dnds.csv"
-    records = AlignIO.read(alnfile, "phylip-relaxed")
-    omegas = [] # list of lists, where first entry is dnds from codon frequencies and second entry is dnds from amino acid frequencies
+def derive_site_dnds(site, records, mu_dict):
     
-    for i in range(0, len(records[0]), 3):
-        freq_dict = dict(zip(codons, np.repeat(0., 61)))
-        freq_dict_from_aa = {}  # dict(zip(codons, np.repeat(0., 61)))
-        
-        column = list(records[:, i:i+3])
-        length = float(len(column))
-        
-        for row in column:
-            codon = str(row.seq)
-            freq_dict[codon] += 1./length
-        
-        for aa_family in genetic_code:
-            total_aa = 0.
-            for entry in aa_family:
-                total_aa += freq_dict[entry]
-            total_aa /= float(len(aa_family))
-            for entry in aa_family:
-                freq_dict_from_aa[entry] = total_aa
-        dnds1 = compute_dnds(freq_dict)
-        dnds2 = compute_dnds(freq_dict_from_aa)
-        omegas.append( [dnds1, dnds2] )                     
+    freq_dict = dict(zip(g.codons, np.zeros(61)))
+    freq_dict_from_aa = {}  # dict(zip(codons, np.repeat(0., 61)))
     
+    # Ignore gaps or noncanonical in column
+    column = []
+    column_raw = list(records[:, site:site+3])
+    for entry in column_raw:
+        codon_current = str(entry.seq)
+        assert(codon_current not in g.stop_codons), "WOAH, STOP CODON!"
+        if codon_current in g.codons:
+            column.append(codon_current)
+    length = len(column)
+    
+    for codon in column:
+        freq_dict[codon] += 1./length
+    
+    for aa_family in g.genetic_code:
+        total_aa = 0.
+        for entry in aa_family:
+            total_aa += freq_dict[entry]
+        total_aa /= float(len(aa_family))
+        for entry in aa_family:
+            freq_dict_from_aa[entry] = total_aa
+           
+    dnds_c = compute_dnds(freq_dict, mu_dict)       
+    dnds_a = compute_dnds(freq_dict_from_aa, mu_dict)       
+
+    return dnds_c, dnds_a
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+seqdir = "../data/"
+outdir = "../results/"
+hydir = "../results/"
+datasets = ["amine", "ATP-cone", "COXI", "Transketolase_C", "camelid", "pb2"]
+
+
+for data in datasets:
+    outfile = outdir + data + "_dnds_from_alignment.txt"
+    alnfile = seqdir + data + ".fasta"
+    
+    hyphile = hydir + data + "_swmutsel_hyout.txt"
+    if not os.path.exists(hyphile):
+        continue
+    mu_dict = extract_mutation_rates(hyphile)
+    
+    records = AlignIO.read(alnfile, "fasta")
+    omegas = []
+    
+    for i in range(len(records[0])/3):
+        print "  ",i
+        site = i*3
+        omegas.append( derive_site_dnds(site, records, mu_dict) )
+    
+        
     with open(outfile, "w") as outf:
-        outf.write("site,dnds_pred_from_codon,dnds_pred_dnds_from_aa\n")
+        outf.write("site,dnds_codon,dnds_aa\n")
         for i in range(len(omegas)):
             outf.write(str(i) + "," + str(omegas[i][0]) + "," + str(omegas[i][1]) + "\n")
-
-# 
-# roots = ["rootrandom", "rootmax", "rootmin"]
-# syns  = ["synsel", "nosynsel"]
-# ns    = ["n5","n6","n7","n8", "n9", "n10"]
-# bls   = ["bl0.005", "bl0.01", "bl0.02", "bl0.04", "bl0.08", "bl0.16"]
-# job_names = []
-# total_runs = len(roots) * len(syns) * len(ns) * len(bls)
-# 
-# 
-# 
-# for roottype in roots:
-#     for s in syns:
-#         for n in ns:
-#             for bl in bls:
-#                 name = n + "_" + bl + "_" + s + "_" + roottype
-#                     job_names.append(name)
-# 
-job_names = ["n9_bl0.16"]
-total_runs = 1
-completed_runs = 0
-while (completed_runs < total_runs):
-    if (total_runs - completed_runs) >= numproc:
-        jobs = []
-        for i in range(completed_runs, completed_runs + numproc):
-            name = job_names[i]
-            print name
-            p = multiprocessing.Process(target = multi_worker, args = (name,))
-            jobs.append(p)
-            p.start()
-        for j in jobs:
-            j.join()
-        completed_runs += numproc
     
-    else:
-        jobs = []
-        for i in range(total_runs - completed_runs - 1, total_runs):
-            name = job_names[i]
-            p = multiprocessing.Process(target = multi_worker, args = (name,))
-            jobs.append(p)
-            p.start()
-        for j in jobs:
-            j.join()
-        break            
-         
-
-                
-                
-                
-                
-                
-                
-                
                 
                         
 
