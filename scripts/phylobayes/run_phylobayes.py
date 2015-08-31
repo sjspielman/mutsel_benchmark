98c16aa8fa225d6db769e6f0541657cb3e32e611
Stephanie Spielman <stephanie.spielman@gmail.com>
2015-08-31 12:44:12 -0500

    some function reorganization and cleanup. will likely need runtime debug

--- scripts/phylobayes/run_phylobayes.py
+++ scripts/phylobayes/run_phylobayes.py
@@ -16,68 +16,9 @@ from Bio import AlignIO
 from dendropy import Tree
 from pyvolve import *
 from compute_dnds_from_mutsel import *
+from parsing_functions import *
 g = Genetics()
-        
-
-def run_pb(alnfile, treefile, cpu, job_name, every = '5', until = '1100', burnin = '100', restart=False):
-    '''
-        NOTE: Assumes that pb_mpi and readpb_mpi executables are in the current directory.
-    '''
-    # Run phylobayes for 5500 generations, sampling every 5, producing posterior distribution of n = 1100
-    # use this command if restarting from a trace file
-    if restart:
-        
-    # use this command if from scratch
-    else:
-        
-    
-
-
-def parse_pb(cpu, job_name, burnin = '100'):
-    '''
-        Parse tracefile. Return site-wise amino acid fitnesses as list: [ [site1_fitnesses], [site2_fitnesses], [site3_fitnesses] ... [siten_fitnesses] ], and return dictionary of mutation rates.
-        Phylobayes chain will have had a length of 5500 and been sampled every 5, resulting in a sample size of 1100. 
-        Results are processed here with a burnin of 100, leaving a posterior sample size of 1000. 
-
-    '''
-    # Parse phylobayes output.
-    
-    readpb_call = "mpirun -np " + str(cpu) + " ./readpb_mpi -x " + str(burnin) + " 1 -1 " + job_name + "\n"
-    print readpb_call
-    run_readpb_call = subprocess.call(readpb_call, shell = True)
-    assert( run_readpb_call == 0 ), "readpb_mpi didn't run!"
-
-    # Read in fitness values from .aap file
-    fitness = np.loadtxt(job_name + ".aap")
-    
-    # Grab posterior means for mutation rates from the .trace file, using same sampling as for fitnesses
-    mu_dict = parse_trace_mutation( job_name, burnin )
-    
-    return fitness, mu_dict
-
-
-
-def parse_phylobayes_tracefile(job_name, burnin = 100):
-    ''' 
-        Parse the .trace file returned by phylobayes to obtain mutation rates
-        ORDER (starts at field 10, index from 0): fA fC fG fT nucrrAC nucrrAG nucrrAT nucrrCG nucrrCT nucrrGT
-    '''
-   
-    # Load trace file, but skip header + burnin and keep only relevant columns (mutational parameters)
-    params = np.loadtxt(file, skiprows = int(burnin) + 1, usecols = range(10,20)) 
-    post_mean = np.mean(params, axis = 0) # Posterior means for desired parameters
-    # Assign post_mean values w/ dictionaries to avoid bugs
-    raw_keys = ["A", "C", "G", "T", "AC", "AG", "AT", "CG", "CT", "GT"]
-    raw_mu   = dict(zip(raw_keys, post_mean)) 
-    keys = ["AC", "CA", "AG", "GA", "AT", "TA", "CG", "GC", "CT", "TC", "GT", "TG"]
-    mu = {}
-    for key in keys:
-        targetfreq = raw_mu[ key[1] ]
-        exch = raw_mu[ "".join(sorted(key)) ]
-        mu[key] = targetfreq * exch
-    return mu
-    
-
+ 
 
 def main():
     
@@ -120,17 +61,7 @@ def main():
     # For this, we need to build a MutSel model to obtain equilibrium frequences (as mu not symmetric) from left eigenvector
     dnds = []
     for site_fitness in sitewise_fitness:
-    
-        # Build the MutSel matrix (assumes equal codon frequencies per amino acid) with these parameters and extract equilibrium codon frequencies
-        params = {"mu": mu_dict, "fitness": site_fitness}
-        matbuilder = mutSel_Matrix(params, "neutral")
-        matrix = matbuilder()
-        eqfreqs = matbuilder.extract_state_freqs(matrix)
-        cf = dict(zip(g.codons, eqfreqs))
-        
-        # Derive dN/dS and append to list
-        c = dNdS_from_MutSel(cf, mu_dict)
-        dnds.append( c.compute_dnds()  )   
+        dnds.append(dnds_from_params(site_fitness, mu_dict))
     
     
     # Save dnds file
--- scripts/swmutsel/run_swmutsel.py
+++ scripts/swmutsel/run_swmutsel.py
@@ -17,6 +17,7 @@ import numpy as np
 from Bio import AlignIO
 from compute_dnds_from_mutsel import *
 from pyvolve import *
+from parsing_functions import *
 g = Genetics()
 
 ZERO=1e-8
@@ -30,95 +31,6 @@ penalty = {"nopenal" :"",
            "mvn100"  :" -p mvn,100", 
            "mvn1000" :" -p mvn,1000"
           }
-
-    
-def optimize_tree_only(cpu, pi, kappa):
-    '''
-        Call hyphy batchfile "optimize_fmutsel_neutral_tree_only.bf" to obtain optimized branch lengths for *fixed nucleotide frequencies and kappa*
-        Parse resulting output, and return all parameters (a new newick tree string, freqs, kappa). Note that freqs are returned as an ordered (T,C,A,G) csv string and kappa is also a string.
-        NOTE: Assumes that the executable HYPHYMP is in the current directory.
-    '''
-    
-    # Input the desired parameters to fix
-    with open("optimize_fmutsel_neutral_tree_only.bf", "r") as f:
-        raw = f.read()
-    raw = raw.replace("insert_kappa", str(kappa))
-    keypi = dict(zip(["insert_fA", "insert_fC", "insert_fG", "insert_fT"], pi))
-    for key in keypi:
-        raw = raw.replace(key, str(keypi[key]))
-    with open("optimize_fmutsel_neutral_tree_only.bf", "w") as f:
-        f.write(raw)
-        
-    # Optimize branch lengths
-    run_hyphy = subprocess.call("./HYPHYMP CPU=" + str(cpu) + " optimize_fmutsel_neutral_tree_only.bf > hyout.txt", shell=True)
-    assert(run_hyphy == 0), "optimize_fmutsel_neutral_tree_only.bf did not run!"
-
-    with open("hyout.txt", "r") as hyout:
-        hyout_string = str( hyout.read() )
-        
-    # Tree w/ optimized branch lengths #
-    find_tree = re.search("Tree codon_tree=(.+)$", hyout_string)
-    tree = find_tree.group(1)
-    tree = re.sub(r"Node\d+", r"", tree) # remove node labels
-    #tree = re.sub(r"([XN]P)(\d+)(\d)([XN]M)(\d+)(\d)", r"\1_\2.\3_\4_\5.\6", tree) # add back underscores, periods to taxon labels. This is necessary for the amine dataset and shouldn't match anything else.
-    
-    return tree
-    
-    
-    
-
-def optimize_fmutsel_neutral(cpu):
-    '''
-        Call hyphy batchfile "optimize_fmutsel_neutral.bf" to obtain optimized branch lengths, nucleotide frequencies, and kappa.
-        Parse resulting output, and return all parameters (a new newick tree string, freqs, kappa). Note that freqs are returned as an ordered (T,C,A,G) csv string and kappa is also a string.
-        NOTE: Assumes that the executable HYPHYMP is in the current directory.
-    '''
-    run_hyphy = subprocess.call("./HYPHYMP CPU=" + str(cpu) + " optimize_fmutsel_neutral.bf > hyout.txt", shell=True)
-    assert(run_hyphy == 0), "optimize_fmutsel_neutral.bf did not run!"
-    
-    with open("hyout.txt", "r") as hyout:
-        hyout_string = str( hyout.read() )
-
-    # Nucleotide frequencies #
-    nuc_freqs = np.zeros(4)
-    find_aux_1 = re.search("aux_1=(0\.\d+)", hyout_string)
-    aux_1 = float( find_aux_1.group(1) )
-    find_aux_2 = re.search("aux_2=(0\.\d+)", hyout_string)
-    aux_2 = float( find_aux_2.group(1) )
-    find_aux_3 = re.search("aux_3=(0\.\d+)", hyout_string)
-    aux_3 = float( find_aux_3.group(1) )
-    nuc_freqs[0] = aux_1
-    nuc_freqs[1] = (1. - aux_1) * aux_2
-    nuc_freqs[2] = (1. - aux_1) * (1. - aux_2) * aux_3
-    nuc_freqs[3] = (1. - aux_1) * (1. - aux_2) * (1. - aux_3)
-    assert( abs(1. - np.sum(nuc_freqs)) < ZERO ), "Nucleotide frequencies do not sum to 1!"
-    nuc_freqs_ordered = nuc_freqs[[3,1,0,2]]
-
-    # Kappa #
-    find_kappa = re.search("k=(\d+\.\d+)", hyout_string)
-    kappa = find_kappa.group(1)
-    
-    # Mutation dictionary #
-    mu_dict = build_mu_dict(nuc_freqs, float(kappa) )
-    
-    # Tree w/ optimized branch lengths #
-    find_tree = re.search("Tree codon_tree=(.+)$", hyout_string)
-    tree = find_tree.group(1)
-    tree = re.sub(r"Node\d+", r"", tree) # remove node labels
-    #tree = re.sub(r"([XN]P)(\d+)(\d)([XN]M)(\d+)(\d)", r"\1_\2.\3_\4_\5.\6", tree) # add back underscores, periods to taxon labels. This is necessary for the amine dataset and shouldn't match anything else.
-
-    return nuc_freqs_ordered, kappa, tree, mu_dict
-
-
-
-def build_mu_dict(pi, kappa):
-    ''' Construct mutation rates dictionary.'''
-    a = pi[0]
-    c = pi[1]
-    g = pi[2]
-    t = pi[3]
-    mu = {'AG':kappa*g, 'TC':kappa*c, 'GA':kappa*a, 'CT':kappa*t, 'AC':c, 'TG':g, 'CA':a, 'GT':t, 'AT':t, 'TA':a, 'GC':c, 'CG':g}  
-    return mu
     
 
 def run_swmutsel(pi, kappa, alnfile, treefile, cpu, dataset, penalname, penalarg):
@@ -169,7 +81,10 @@ def main():
     
     # Prep hyphy input file and call hyphy to optimize mutational parameters, create mu_dict to use later for dnds derivation, and make a treefile with the optimized tree
     subprocess.call("cat " + alnfile_fasta + " " + treefile + " > hyin.txt", shell=True)
-    pi, kappa, treestring, mu_dict = optimize_fmutsel_neutral(cpu)       
+    run_hyphy = subprocess.call("./HYPHYMP CPU=" + str(cpu) + " optimize_fmutsel_neutral.bf > hyout.txt", shell=True)
+    assert(run_hyphy == 0), "optimize_fmutsel_neutral.bf did not run!"
+    
+    pi, kappa, treestring, mu_dict = extract_optimized_params("hyout.txt")       
     pi2 = [str(i) for i in pi]
     pi_string = ",".join(pi2)             
     with open(opt_treefile, "w") as f:
@@ -183,18 +98,8 @@ def main():
         # Compute site-wise dN/dS from amino-acid fitness values
         dnds = []
         for site_fitness in sitewise_fitness:
+            dnds.append(dnds_from_params(site_fitness, mu_dict))
             
-            # Build the MutSel matrix (assumes equal codon frequencies per amino acid) with these parameters and extract equilibrium codon frequencies
-            params = {"mu": mu_dict, "fitness": site_fitness}
-            matbuilder = mutSel_Matrix(params, "neutral")
-            matrix = matbuilder()
-            eqfreqs = matbuilder.extract_state_freqs(matrix)
-            cf = dict(zip(g.codons, eqfreqs))
-            
-            # Derive dN/dS and append to list
-            c = dNdS_from_MutSel(cf, mu_dict)
-            dnds.append( c.compute_dnds()  )    
-
         # Save
         with open(dataset + "_" + penal + "_dnds.txt", "w") as outf:
             outf.write( "\n".join([str(i) for i in dnds]) )
