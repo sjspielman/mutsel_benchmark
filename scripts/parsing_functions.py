fd130287c451db13ac9b0eea47f58a216c63de78
Stephanie Spielman <stephanie.spielman@gmail.com>
2015-09-01 08:55:08 -0500

    file with all parsing functions

---
+++ scripts/parsing_functions.py
@@ -0,0 +1,153 @@
+# Various functions used to parse swMutSel, pbMutSel
+
+
+import re
+import os
+import sys
+import subprocess
+import numpy as np
+from Bio import AlignIO
+from dendropy import Tree
+from pyvolve import *
+from compute_dnds_from_mutsel import *
+g = Genetics()
+
+
+
+def codon_to_aa_freqs(codonfreqs):
+    cf = dict(zip(g.codons, codonfreqs))
+    aa_freqs = []
+    for family in g.genetic_code:
+        total = 0.
+        for syn in family:
+            total += cf[syn]
+        total /= float(len(family))
+        aa_freqs.append(total)
+        
+    return aa_freqs
+        
+
+def codon_freqs_from_fitness(fitness, mu):
+    params = {"fitness": fitness, "mu": mu}
+    matbuilder = mutSel_Matrix(params, scale_matrix = "neutral")
+    matrix = matbuilder()
+    eqfreqs = matbuilder.extract_state_freqs(matrix)
+    return eqfreqs
+
+
+
+def dnds_from_params(site_fitness, mu_dict):
+
+    # Build the MutSel matrix (assumes equal codon frequencies per amino acid) with these parameters and extract equilibrium codon frequencies
+    eqfreqs = codon_freqs_from_fitness(site_fitness, mu_dict)
+    cf = dict(zip(g.codons, eqfreqs))
+
+    # Derive dN/dS and return
+    c = dNdS_from_MutSel(cf, mu_dict)
+    return c.compute_dnds()
+        
+
+
+
+
+def build_mu_dict(pi, kappa):
+    ''' Construct mutation rates dictionary.'''
+    a = pi[0]
+    c = pi[1]
+    g = pi[2]
+    t = pi[3]
+    mu = {'AG':kappa*g, 'TC':kappa*c, 'GA':kappa*a, 'CT':kappa*t, 'AC':c, 'TG':g, 'CA':a, 'GT':t, 'AT':t, 'TA':a, 'GC':c, 'CG':g}  
+    return mu
+    
+    
+
+def extract_optimized_params(infile, return_tree = True):
+    '''
+        Parse results from hyphy batchfile "optimize_fmutsel_neutral.bf" run.  Return all parameters (a new newick tree string, freqs, kappa). Note that freqs are returned as an ordered (T,C,A,G) csv string and kappa is also a string.
+        NOTE: Assumes that the executable HYPHYMP is in the current directory.
+    '''    
+    with open(infile, "r") as hyout:
+        hyout_string = str( hyout.read() )
+
+    # Nucleotide frequencies #
+    nuc_freqs = np.zeros(4)
+    find_aux_1 = re.search("aux_1=(0\.\d+)", hyout_string)
+    aux_1 = float( find_aux_1.group(1) )
+    find_aux_2 = re.search("aux_2=(0\.\d+)", hyout_string)
+    aux_2 = float( find_aux_2.group(1) )
+    find_aux_3 = re.search("aux_3=(0\.\d+)", hyout_string)
+    aux_3 = float( find_aux_3.group(1) )
+    nuc_freqs[0] = aux_1
+    nuc_freqs[1] = (1. - aux_1) * aux_2
+    nuc_freqs[2] = (1. - aux_1) * (1. - aux_2) * aux_3
+    nuc_freqs[3] = (1. - aux_1) * (1. - aux_2) * (1. - aux_3)
+    assert( abs(1. - np.sum(nuc_freqs)) < ZERO ), "Nucleotide frequencies do not sum to 1!"
+    nuc_freqs_ordered = nuc_freqs[[3,1,0,2]]
+
+    # Kappa #
+    find_kappa = re.search("k=(\d+\.\d+)", hyout_string)
+    kappa = find_kappa.group(1)
+    
+    # Mutation dictionary #
+    mu_dict = build_mu_dict(nuc_freqs, float(kappa) )
+    
+    if return_tree:
+        # Tree w/ optimized branch lengths #
+        find_tree = re.search("Tree codon_tree=(.+)$", hyout_string)
+        tree = find_tree.group(1)
+        tree = re.sub(r"Node\d+", r"", tree) # remove node labels
+        #tree = re.sub(r"([XN]P)(\d+)(\d)([XN]M)(\d+)(\d)", r"\1_\2.\3_\4_\5.\6", tree) # add back underscores, periods to taxon labels. This is necessary for the amine dataset and shouldn't match anything else.
+
+        return nuc_freqs_ordered, kappa, tree, mu_dict
+    
+    else:
+        return nuc_freqs_ordered, kappa, mu_dict
+
+
+    
+
+
+def parse_pb(cpu, job_name, burnin = '100'):
+    '''
+        Parse tracefile. Return site-wise amino acid fitnesses as list: [ [site1_fitnesses], [site2_fitnesses], [site3_fitnesses] ... [siten_fitnesses] ], and return dictionary of mutation rates.
+        Phylobayes chain will have had a length of 5500 and been sampled every 5, resulting in a sample size of 1100. 
+        Results are processed here with a burnin of 100, leaving a posterior sample size of 1000. 
+
+    '''
+    # Parse phylobayes output.
+    
+    readpb_call = "mpirun -np " + str(cpu) + " ./readpb_mpi -x " + str(burnin) + " 1 -1 " + job_name + "\n"
+    print readpb_call
+    run_readpb_call = subprocess.call(readpb_call, shell = True)
+    assert( run_readpb_call == 0 ), "readpb_mpi didn't run!"
+
+    # Read in fitness values from .aap file
+    fitness = np.loadtxt(job_name + ".aap")
+    
+    # Grab posterior means for mutation rates from the .trace file, using same sampling as for fitnesses
+    mu_dict = parse_trace_mutation( job_name + ".trace", burnin )
+    
+    return fitness, mu_dict
+
+
+
+def parse_trace_mutation(file, job_name, burnin = 100):
+    ''' 
+        Parse the .trace file returned by phylobayes to obtain mutation rates
+        ORDER (starts at field 10, index from 0): fA fC fG fT nucrrAC nucrrAG nucrrAT nucrrCG nucrrCT nucrrGT
+    '''
+   
+    # Load trace file, but skip header + burnin and keep only relevant columns (mutational parameters)
+    params = np.loadtxt(file, skiprows = int(burnin) + 1, usecols = range(10,20)) 
+    post_mean = np.mean(params, axis = 0) # Posterior means for desired parameters
+    # Assign post_mean values w/ dictionaries to avoid bugs
+    raw_keys = ["A", "C", "G", "T", "AC", "AG", "AT", "CG", "CT", "GT"]
+    raw_mu   = dict(zip(raw_keys, post_mean)) 
+    keys = ["AC", "CA", "AG", "GA", "AT", "TA", "CG", "GC", "CT", "TC", "GT", "TG"]
+    mu = {}
+    for key in keys:
+        targetfreq = raw_mu[ key[1] ]
+        exch = raw_mu[ "".join(sorted(key)) ]
+        mu[key] = targetfreq * exch
+    return mu
+    
--- scripts/postprocessing/plot_results.R
+++ scripts/postprocessing/plot_results.R
@@ -10,6 +10,28 @@ results %>% filter(type == "simulation") -> sim.results
 results %>% filter(type == "empirical") -> emp.results
 
 
+#### 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 ##### Correlation boxplots for simulated data #####
 simcorrs <- NULL
@@ -19,7 +41,7 @@ i <- 1
 for (dataname in simnames){
     newdname <- strsplit(dataname, "_")[[1]][1]
     print(newdname)
-    temp <- results.spread %>% filter(dataset == dataname) %>% select(-dataset, -type, -site) %>% na.omit()
+    temp <- results.spread %>% filter(dataset == dataname, fel1 != 1) %>% select(-dataset, -type, -site) %>% na.omit()
     corrs <- as.matrix(cor(temp)) %>% melt() %>% filter(Var1 %in% c("empirical", "true", "fel1"), value != 1) %>% arrange(Var1)
     corrs$dataset <- newdname
     colnames(corrs)[3] <- "r"
@@ -28,26 +50,36 @@ for (dataname in simnames){
     }
     else{
         simcorrs <- rbind(simcorrs, corrs)
-        #colnames(simcorrs)[i+3] <- newdname
     }
     first <- FALSE
     i <- i+1
 }
 simcorrs$Var2 <-  factor(simcorrs$Var2, levels = c("true", "fel1", "empirical", "nopenal", "mvn10", "mvn100", "mvn1000", "d0.01", "d0.1", "d1.0"))
-simcorrs %>% filter(Var1 == "true") %>% ggplot(aes(x = Var2, y = r)) + geom_boxplot() + xlab("Inference method") + ylab("Pearson Correlation")
+simcorrs %>% filter(Var1 == "true", Var2 %in% c("fel1", "empirical", "nopenal", "mvn10", "d1.0") %>% ggplot(aes(x = Var2, y = r)) + geom_boxplot() + xlab("Inference method") + ylab("Pearson Correlation")
 
 
-##### Scatterplots for a representative dataset #####
-for (dataname in simnames){
-    results.spread %>% filter(dataset == dataname)  -> plotme
-    p1 <- ggplot(plotme, aes(x = true, y = fel1)) + geom_point(alpha=0.6) + geom_abline(slope = 1, intercept = 0, color="red") + xlab("True dN/dS") + ylab("FEL1") + ggtitle(dataname)
-    p2 <- ggplot(plotme, aes(x = true, y = empirical)) + geom_point(alpha=0.6) + geom_abline(slope = 1, intercept = 0, color="red") + xlab("True dN/dS") + ylab("Empirical dN/dS")
-    p3 <- ggplot(plotme, aes(x = true, y = nopenal)) + geom_point(alpha=0.6) + geom_abline(slope = 1, intercept = 0, color="red") + xlab("True dN/dS") + ylab("swMutSel nopenal dN/dS")
-    plot.new()
-    p<-plot_grid(p1, p2, p3, nrow=1)
-    p
-    readline("enter to continue")
-}
+##### Scatterplots for a representative dataset, 2BCG_Y_simulated #####
+representative_sim <- "2BCG_Y_simulated"
+results.spread %>% filter(dataset == representative_sim, fel1 != 1)  -> plotme # fel1 are not informative.
+r.true_fel <- round(cor(plotme$true, plotme$fel1),2)
+r.true_empirical <- round(cor(plotme$true, plotme$empirical),2)
+r.fel_empirical <- round(cor(plotme$fel1, plotme$empirical),2)
+r.true_nopenal <- round(cor(plotme$true, plotme$nopenal),2)
+r.true_mvn10 <- round(cor(plotme$true, plotme$mvn10),2)
+r.true_d1.0 <- round(cor(plotme$true, plotme$d1.0),2)
+#r.true_pb <- round(cor(plotme$true, plotme$pb),2)
+p1 <- ggplot(plotme, aes(x = true, y = fel1)) + geom_point(alpha=0.6) + geom_abline(slope = 1, intercept = 0, color="red") + xlab("True dN/dS") + ylab("FEL1") + ggtitle(paste(dataname,r))
+p2 <- ggplot(plotme, aes(x = true, y = empirical)) + geom_point(alpha=0.6) + geom_abline(slope = 1, intercept = 0, color="red") + xlab("True dN/dS") + ylab("Empirical dN/dS")
+p3 <- ggplot(plotme, aes(x = true, y = nopenal)) + geom_point(alpha=0.6) + geom_abline(slope = 1, intercept = 0, color="red") + xlab("True dN/dS") + ylab("swMutSel nopenal dN/dS")
+plot.new()
+p<-plot_grid(p1, p2, p3, nrow=1)
+print(p)
+
+
+
+
+
+
 
 
 
@@ -76,14 +108,24 @@ for (dataname in empnames){
     i <- i+1
 }
 empcorrs$Var2 <-  factor(empcorrs$Var2, levels = c("fel1", "empirical", "nopenal", "mvn10", "mvn100", "mvn1000", "d0.01", "d0.1", "d1.0"))
-empcorrs %>% filter(Var1 == "empirical") %>% ggplot(aes(x = Var2, y = r)) + geom_boxplot() + xlab("Inference method") + ylab("Pearson Correlation") # outliers are either amine or pb2
+empcorrs %>% filter(Var1 == "fel1", Var2 %in% c("empirical", "nopenal", "mvn10", "d1.0")) %>% ggplot(aes(x = Var2, y = r)) + geom_boxplot() + xlab("Inference method") + ylab("Pearson Correlation") # outliers are either amine or pb2
+
+
+
 
 
 ##### Scatterplots for a representative dataset #####
-d <- "PF04055"
+d <- "PF07715"
 results.spread %>% filter(dataset == d)  -> plotme
-p1 <- ggplot(plotme, aes(x = fel1, y = empirical)) + geom_point(alpha=0.6) + geom_abline(slope = 1, intercept = 0, color="red") + xlab("FEL1 dN/dS") + ylab("Empirical dN/dS") + ggtitle(d)
+p1 <- ggplot(plotme, aes(x = fel1, y = empirical)) + geom_point(alpha=0.6) + geom_abline(slope = 1, intercept = 0, color="red") + xlab("FEL1 dN/dS") + ylab("Empirical dN/dS") + ggtitle(d) + scale_y_log10() + scale_x_log10()
 p2 <- ggplot(plotme, aes(x = fel1, y = nopenal)) + geom_point(alpha=0.6) + geom_abline(slope = 1, intercept = 0, color="red") + xlab("FEL1 dN/dS") + ylab("swMutSel nopenal dN/dS")
 p3 <- ggplot(plotme, aes(x = empirical, y = nopenal)) + geom_point(alpha=0.6) + geom_abline(slope = 1, intercept = 0, color="red") + xlab("Empirical dN/dS") + ylab("swMutSel nopenal dN/dS")
 plot_grid(p1, p2, p3, nrow=1)
+
+
+
+
+
+
+
 
