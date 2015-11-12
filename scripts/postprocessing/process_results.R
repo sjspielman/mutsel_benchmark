c33349d60c355b0f4456d747fa2f7480888b7316
Stephanie J Spielman <stephanie.spielman@gmail.com>
2015-11-12 11:16:35 -0600

    begin to update for new empirical

--- scripts/postprocessing/process_results.R
+++ scripts/postprocessing/process_results.R
@@ -6,9 +6,9 @@ require(tidyr)
 
 datadir_parent <- "../../results/"
 sim.datasets   <- c("1B4T_A_simulated", "1G58_B_simulated", "1GV3_A_simulated", "1HUR_A_simulated", "1IBS_A_simulated", "1PV1_A_simulated", "1QMV_A_simulated", "1R6M_A_simulated", "1V9S_B_simulated", "1W7W_B_simulated", "1X1O_B_simulated", "1YPI_A_simulated", "1ZNN_A_simulated", "2A84_A_simulated", "2BCG_Y_simulated", "2CFE_A_simulated", "2CJM_C_simulated", "2CNV_A_simulated", "2FLI_A_simulated", "2G0N_B_simulated")
-emp.datasets   <- c("pb2", "PF00126", "PF00593", "PF01266", "PF01336", "PF01926", "PF02518", "PF04055", "PF07715")
+emp.datasets   <- c("PF00106", "PF00149", "PF00188", "PF00300", "PF00512", "PF00753", "PF01261", "PF01551", "PF01636", "PF03144", "PF00141", "PF00158", "PF00226", "PF00482", "PF00520", "PF01061", "PF01546", "PF01584", "PF02775", "PF04542", "PF00168", "PF00486", "PF00535", "PF01590", "PF03466", "PF00271", "PF00501", "PF00571", "PF00593", "PF00126", "PF01266", "PF01336", "PF01926", "PF02518", "PF04055", "PF07715", "pb2")
 datasets       <- c(sim.datasets, emp.datasets)
-meths          <- c("nopenal", "d0.01", "d0.1", "d1.0", "mvn10", "mvn100", "mvn1000", "phylobayes")
+fullmeths      <- c("nopenal", "d0.01", "d0.1", "d1.0", "mvn10", "mvn100", "mvn1000", "phylobayes")
 
 
 
@@ -24,16 +24,17 @@ for (dataset in datasets){
     
     if (grepl("_simulated",dataset)){
         type <- "simulation"
-        
+        meths <- fullmeths  
     }
     else{
         type <- "empirical"
+        meths <- c("nopenal", "phylobayes")
     }
     datadir  <- paste0(datadir_parent,type,"/")
 
     # dN/dS from SLAC 
     dat <- read.table(paste0(datadir, dataset, "_SLAC.txt"), header=T)
-    datdnds <- (dat$dN)/(mean(dat$dS))
+    datdnds <- dat$dN/mean(dat$dS)
     l <- nrow(dat)
     temp <- data.frame("dataset" = dataset, "site" = 1:l, "dnds" = datdnds , "method" = "slac", type = type)
     results.dnds <- rbind(results.dnds, temp)
@@ -75,7 +76,6 @@ for (dataset in datasets){
 #         
 }
 write.csv(results.dnds, paste0(datadir_parent, "dnds_results.csv", sep=""), row.names=FALSE, quote=FALSE)
-
 
 
 
