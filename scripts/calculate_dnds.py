433aaec10007a0f939c7ec20c138e06981ce0011
sjspielman <stephanie.spielman@utexas.edu>
2015-11-21 17:21:00 -0600

    runtime debug

--- scripts/calculate_dnds.py
+++ scripts/calculate_dnds.py
@@ -10,7 +10,8 @@ from universal_functions import *
 emp_datasets =  ["PF00106", "PF00149", "PF00188", "PF00300", "PF00512", "PF00753", "PF01261", "PF01551", "PF01636", "PF03144", "PF00141", "PF00158", "PF00226", "PF00482", "PF00520", "PF01061", "PF01546", "PF01584", "PF02775", "PF04542", "PF00168", "PF00486", "PF00535", "PF01590", "PF03466", "PF00271", "PF00501", "PF00571", "PF00593", "PF00126", "PF01266", "PF01336", "PF01926", "PF02518", "PF04055", "PF07715", "pb2"] 
 sim_datasets = ["1B4T_A_simulated", "1G58_B_simulated", "1GV3_A_simulated", "1HUR_A_simulated", "1IBS_A_simulated", "1PV1_A_simulated", "1QMV_A_simulated", "1R6M_A_simulated", "1V9S_B_simulated", "1W7W_B_simulated", "1X1O_B_simulated", "1YPI_A_simulated", "1ZNN_A_simulated", "2A84_A_simulated", "2BCG_Y_simulated", "2CFE_A_simulated", "2CJM_C_simulated", "2CNV_A_simulated", "2FLI_A_simulated", "2G0N_B_simulated"]
 datasets =  {"empirical":emp_datasets, "simulation":sim_datasets}
-methods = [{"phylobayes":"phylobayes"}, {"swmutsel": "mvn10", "mvn100", "mvn1000", "d1.0", "d0.1", "d0.01", "nopenal"}]
+methods = {"phylobayes": ["phylobayes"], "swmutsel": ["mvn10", "mvn100", "mvn1000", "d1.0", "d0.1", "d0.01", "nopenal"]}
+families = methods.keys()
 
 
 
@@ -20,20 +21,26 @@ for datatype in datasets:
     outdir = resdir_raw + "derived_dnds/"
     
     for data in datasets[datatype]:        
-        for family in methods:
+        for family in families:
 
             resdir = resdir_raw + family + "/"   
                   
-            for method in family:
-                
+            for method in methods[family]:
+
                 outfile = outdir + data + "_" + method + "_dnds.txt"
+                if os.path.exists(outfile):
+                    continue
+                    
                 print "Calculating dN/dS for", data, method
             
                 ######## OBTAIN FITNESS VALUES AND MUTATION RATES ########
                 if method == "phylobayes":
-                    fitness = np.loadtxt(resdir + data + "_phylobayes.aap")
-                    fitness = np.log(fitness)
-                    mu_dict = parse_pbMutSel_mutation(resdir + data + "_phylobayes.trace")
+                    try:
+                        fitness = np.loadtxt(resdir + data + "_phylobayes.aap")
+                        fitness = np.log(fitness)
+                        mu_dict = parse_pbMutSel_mutation(resdir + data + "_phylobayes.trace")
+                    except:
+                        pass
                 else:
                     fitness = np.loadtxt(resdir + data + "_" + method + "_fitness.txt")
                     mu_dict = parse_swMutSel_mutation(resdir + data + "_" + method + "_MLE.txt")   
