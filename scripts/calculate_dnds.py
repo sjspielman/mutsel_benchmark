5404306e7bcb31af0680a7ac2527edbed447aea5
Stephanie Spielman <stephanie.spielman@gmail.com>
2015-11-24 15:11:16 -0600

    update, will need more

--- scripts/calculate_dnds.py
+++ scripts/calculate_dnds.py
@@ -6,15 +6,44 @@ import numpy as np
 from universal_functions import *
 
 
-emp_datasets =  ["PF00106", "PF00149", "PF00188", "PF00300", "PF00512", "PF00753", "PF01261", "PF01551", "PF01636", "PF03144", "PF00141", "PF00158", "PF00226", "PF00482", "PF00520", "PF01061", "PF01546", "PF01584", "PF02775", "PF04542", "PF00168", "PF00486", "PF00535", "PF01590", "PF03466", "PF00271", "PF00501", "PF00571", "PF00593", "PF00126", "PF01266", "PF01336", "PF01926", "PF02518", "PF04055", "PF07715", "pb2"] 
+emp_datasets =  ["PF00106", "PF00149", "PF00188", "PF00300", "PF00512", "PF00753", "PF01261", "PF01551", "PF01636", "PF03144", "PF00141", "PF00158", "PF00226", "PF00482", "PF00520", "PF01061", "PF01546", "PF01584", "PF02775", "PF04542", "PF00168", "PF00486", "PF00535", "PF01590", "PF03466", "PF00271", "PF00501", "PF00571", "PF00593", "PF00126", "PF01266", "PF01336", "PF01926", "PF02518", "PF04055", "PF07715"] 
 sim_datasets = ["1B4T_A_simulated", "1G58_B_simulated", "1GV3_A_simulated", "1HUR_A_simulated", "1IBS_A_simulated", "1PV1_A_simulated", "1QMV_A_simulated", "1R6M_A_simulated", "1V9S_B_simulated", "1W7W_B_simulated", "1X1O_B_simulated", "1YPI_A_simulated", "1ZNN_A_simulated", "2A84_A_simulated", "2BCG_Y_simulated", "2CFE_A_simulated", "2CJM_C_simulated", "2CNV_A_simulated", "2FLI_A_simulated", "2G0N_B_simulated"]
 datasets =  {"empirical":emp_datasets, "simulation":sim_datasets}
 methods = {"swmutsel": ["mvn10", "mvn100", "mvn1000", "d1.0", "d0.1", "d0.01", "nopenal"], "phylobayes": ["phylobayes"]}
 families = methods.keys()
 
 
+def extract_info(directory, data, type):
+    '''
+        Extract fitness and mutation rates from a given inference.
+    '''
+    fitness = None
+    mu_dict = None
+    if type == "phylobayes":
+        fitness = np.loadtxt(directory + "phylobayes/" + data + "_phylobayes.aap")
+        fitness = np.log(fitness)
+        mu_dict = parse_pbMutSel_mutation(directory + "phylobayes/" + data + "_phylobayes.trace")
+    else:
+        fitness = np.loadtxt(directory + "swmutsel/" + data + "_" + type + "_fitness.txt")
+        mu_dict = parse_swMutSel_mutation(directory + "swmutsel/" + data + "_" + type + "_MLE.txt")   
+
+    assert(fitness is not None), "\n Could not retrieve fitness values."
+    assert(mu_dict is not None), "\n Could not retrieve mutation rates."
+    return fitness, mu_dict
+    
+    
+
+def calculate_save_dnds(fitness, mu_dict, outfile):
+    dnds = []
+    for sitefitness in fitness:
+        dnds.append( dnds_from_params(sitefitness, mu_dict) )     
+    with open(outfile, "w") as outf:
+        outf.write( "\n".join([str(i) for i in dnds]) )
+
+
 notdone = ["PF00501"]
 
+# For simulated yeast, empirical
 for datatype in datasets:
     
     resdir_raw = "../results/raw_results/" + datatype + "/"
@@ -33,27 +62,50 @@ for datatype in datasets:
                     continue
                     
                 print "Calculating dN/dS for", data, method
+                fitness, mu_dict = extract_info(resdir, data, method)
+                calculate_save_dnds(fitness, mu_dict, outfile)
             
-                ######## OBTAIN FITNESS VALUES AND MUTATION RATES ########
-                fitness = None; mu_dict = None; # Part of assertion below to ensure values properly collected
-                if method == "phylobayes":
-                    try:
-                        fitness = np.loadtxt(resdir + data + "_phylobayes.aap")
-                        fitness = np.log(fitness)
-                        mu_dict = parse_pbMutSel_mutation(resdir + data + "_phylobayes.trace")
-                    except:
-                        pass
-                else:
-                    fitness = np.loadtxt(resdir + data + "_" + method + "_fitness.txt")
-                    mu_dict = parse_swMutSel_mutation(resdir + data + "_" + method + "_MLE.txt")   
 
-                assert(fitness is not None), "\n Could not retrieve fitness values."
-                assert(mu_dict is not None), "\n Could not retrieve mutation rates."
 
-                ######## CALCULATE AND SAVE dN/dS ########
-                dnds = []
-                for sitefitness in fitness:
-                    dnds.append( dnds_from_params(sitefitness, mu_dict) )     
-                with open(outfile, "w") as outf:
-                    outf.write( "\n".join([str(i) for i in dnds]) )
-            
+
+# For simulated from empirical parameters, which are named differently.
+resdir = "../results/raw_results/simulated_from_empirical/"
+outdir = resdir + "derived_dnds/"
+for data in emp_datasets:
+
+    outfile = outdir + data + "_simulated_dnds.txt"
+    if os.path.exists(outfile):
+        continue
+                    
+    print "Calculating dN/dS for simulated", data
+    fitness, mu_dict = extract_info(resdir, data, "simulated")
+    calculate_save_dnds(fitness, mu_dict, outfile)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+
+
+
+
+
