100cd81f1ba359b8c9f804927f64304069f66b1a
sjspielman <stephanie.spielman@gmail.com>
2015-12-07 13:23:14 -0600

    updates

--- scripts/simulation/simulate_yeast_alignments.py
+++ scripts/simulation/simulate_yeast_alignments.py
@@ -8,16 +8,16 @@ from numpy import loadtxt
 
 prefix = sys.argv[1]
 freqs = loadtxt("freqs.txt")
-simtree = read_tree(file = "ntaxa1024_bl0.5.tre")
+simtree = read_tree(file = "ntaxa256_bl0.5.tre")
 
 partitions = []
 for row in freqs:
-    m = Model("mutsel", {"state_freqs": row}, scale_matrix="neutral")
+    m = Model("mutsel", {"state_freqs": row})
     p = Partition(models = m, size = 1)
     partitions.append(p)
                         
 # Simulate and save alignment in both fasta, phylip format
 print "Simulating"
 e = Evolver(partitions = partitions, tree = simtree)
-e(seqfile = prefix + "_simulated.fasta", ratefile = None, infofile = None)
-AlignIO.convert(prefix + "_simulated.fasta", "fasta", prefix + "_simulated.phy", "phylip-relaxed")
+e(seqfile = prefix + "_simulated_n256.fasta", ratefile = None, infofile = None)
+AlignIO.convert(prefix + "_simulated_n256.fasta", "fasta", prefix + "_simulated_n256.phy", "phylip-relaxed")
