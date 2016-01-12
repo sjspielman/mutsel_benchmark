ed19f78544bfc6c127ee9fd9d1807476e5471b60
sjspielman <stephanie.spielman@gmail.com>
2016-01-12 10:15:08 -0600

    Now redoing weak with proper smooth distribution

--- scripts/simulation/submit_simulations.sh
+++ scripts/simulation/submit_simulations.sh
@@ -1,7 +1,7 @@
 # Submit simulations to phylocluster:
 
 for DATA in 1B4T_A 1G58_B 1GV3_A 1IBS_A 1R6M_A 1RII_A 1V9S_B 1W7W_B 2BCG_Y 2CFE_A 2FLI_A; do 
-    for DEL in strong weak weakweak; do
+    for DEL in strong weak; do
         NAME=${DATA}_del${DEL}
         qsub simulate_alignments.qsub $NAME
     done
