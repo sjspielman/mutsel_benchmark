ca8363c9cf683a6637eefdac94e42cf748ce5b6e
sjspielman <stephanie.spielman@gmail.com>
2016-01-11 18:52:44 -0600

    weakweak alignments

--- scripts/inference/submit_analysis.sh
+++ scripts/inference/submit_analysis.sh
@@ -5,8 +5,8 @@
 function submit_swmutsel {
     DATA=$1
     TREE=$2
-
-    for M in nopenal mvn1 mvn10 mvn100 d0.01 d0.1 d1.0; do
+    # nopenal mvn1 mvn10 mvn100 d0.01 d0.1 d1.0
+    for M in nopenal; do
         sed -i "s/-N JOB/-N sw_${DATA}_${M}/" swmutsel/swmutsel.qsub
         qsub swmutsel/swmutsel.qsub $TOPDIR $DATA $TREE $M
         sed -i "s/-N sw_${DATA}_${M}/-N JOB/" swmutsel/swmutsel.qsub
@@ -25,7 +25,7 @@ function submit_pbmutsel {
  
 TREE="n512_bl0.5.tre"
 for NAME in 1B4T_A 1G58_B 1GV3_A 1IBS_A 1R6M_A 1RII_A 1V9S_B 1W7W_B 2BCG_Y 2CFE_A 2FLI_A; do 
-    for DEL in strong weak weakweak; do
+    for DEL in strong weak; do
         
         DATA=${NAME}_del${DEL}
         
